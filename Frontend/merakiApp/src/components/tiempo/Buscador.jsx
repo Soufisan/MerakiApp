5913baf45a2da40b6e5ff7488a3c4f39173f3f89
Amanda Gómez Quero <amanda1758@gmail.com>
2025-09-21 23:01:44 +0200

    'masmal'

--- Frontend/merakiApp/src/components/tiempo/Buscador.jsx
+++ Frontend/merakiApp/src/components/tiempo/Buscador.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios'
 import { Button } from 'react-bootstrap';
+import { Boton } from '../boton/Boton';
 
 let APIKEY = 'fea4e10893e2dfaa205a2269eeca82a8';
 
@@ -20,13 +21,15 @@ export const Buscador = ({ setDataTotal }) => {
     } catch (error) {
       console.log(error);
     }
+    setCity(' ');
   };
 
   return (
-    <div>
-      <label>Introduce una ciudad</label>
-      <input type="text" value={city} onChange={handleChange} />
-      <Button onClick={onSubmit}>Aceptar</Button>
+    <div className='d-flex flex-column p-5 justify-content-center  align-items-center gap-2'>
+      <label>¡Introduce una ciudad y comprueba el tiempo!</label>
+      <input classtype="text" value={city} onChange={handleChange} />
+        <Boton onClick={onSubmit} nombre='Aceptar' aspecto='redondo' icono='bi bi-thermometer-sun'/>
+    
     </div>
   );
 };
